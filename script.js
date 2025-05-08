59d87978c98e7151ede9a91542aa9249880192dd
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-08 21:00:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-const canvas = document.getElementById("battlefield");
-const ctx = canvas.getContext("2d");
-
-let mapData = null;
-let tileImages = {};
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-resizeCanvas();
-
-
-fetch("res/maps/test.json")
-  .then (response => response.json())
-  .then (data => {mapData = data;
-    const imgLoader = []
-
-mapData.usedTiles.forEach(function(usedTile){
-    const promise = new Promise(function(resolve){
-      const img = new Image();
-      img.src="res/img/tiles/"+usedTile+".png";
-      img.onload = function(){
-        tileImages[usedTile] = img;
-        resolve ()
-      }
-    } )
-    imgLoader.push(promise)
-} )
-Promise.all(imgLoader).then (function(){
-    drawMap()
-} )
-});
-
-function drawMap() {
-  const tileWidth = 64;
-  const tileHeight = 32;
-
-  for (let y = 0; y < mapData.height; y++) {
-    for (let x = 0; x < mapData.width; x++) {
-      const tileType = mapData.tiles[y][x];
-      const img = tileImages[tileType];
-
-      const screenX = (x - y) * tileWidth / 2 + canvas.width / 2;
-      const screenY = (x + y) * tileHeight / 2;
-
-      ctx.drawImage(img, screenX - tileWidth / 2, screenY);
-    }
+class battlefield {
+  constructor (config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(.battlefield);
+    this.ctx = this.canvas.getContext("2d");
+  }
+  
+  
+  async function mapLoad(map) {
+    const response = await fetch ('maps/${map}.json');
+    const mapData = await response.json();
+    return mapData
   }
 }
-
-document.getElementById("a").setContext= "${(4*1/2)}"
