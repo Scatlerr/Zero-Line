efd0bc02b0b4f0fad15b3b8f9f9a12b9a835a39d
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-08 21:59:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ class battlefield {
     this.ctx = this.canvas.getContext("2d");
   }
   
+  const screenHeight = window.screen.height;
+  const screenWidth = window.screen.width;
+  
+  
   
   async function mapLoad(map) {
-    const response = await fetch ('maps/${map}.json');
+    const response = await fetch (`maps/${map}.json`);
     const mapData = await response.json();
-    return mapData
+    return new Promise(function(resolve){
+      let loadedTiles = 0
+      mapData.usedTiles.forEach(function(usedTile){
+        img = new Image;
+        img.src = `${usedTile}.png`;
+        img.onload = function () {
+          loadedTiles++
+          if (loadedTiles === mapData.usedTiles.length) {
+            resolve(mapData)
+          }
+        }
+      }
+      
+      }
+  }
+  
+  async function drawMap (mapData) {
+    for (x=0, x>=screenWidth, x++) {}
   }
 }
