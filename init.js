57591056b7de42194c9a1dbda6206523ef06c2dd
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-09 12:24:20 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,8 @@
-console
+const battlefield = new Battlefield({
+    element: document,
+    screenHeight: window.screenHeight,
+    screenWidth: window.screenWidth,
+    tileHeight: 32,
+    tileWidth: 64,
+    map: test,
+})
