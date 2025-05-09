5e28ef2e4e6e4afdaec8233c2a4b33f2fc560160
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-09 17:25:07 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
-const battlefield = new Battlefield({
-    element: document,
-    screenHeight: window.innerHeight,
-    screenWidth: window.innerWidth,
-    tileHeight: 32,
-    tileWidth: 64,
-    map: "test",
-})
+document.querySelector("#campaignBtn").onclick(function{
+    const battlefield = new Battlefield({
+        element: document,
+        screenHeight: window.innerHeight,
+        screenWidth: window.innerWidth,
+        tileHeight: 32,
+        tileWidth: 64,
+        map: "test",
+    })
 
-battlefield.mapLoad()
+    battlefield.mapLoad()
+})
