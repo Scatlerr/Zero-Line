9431c6b8a81b8beb62aeed2bf982b13260630279
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-09 21:03:26 +0300

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -1,1 +1,12 @@
-document.querySelectorAll(".mainMenu");
+document.querySelector("#campaignBtn").onclick = function{
+    const battlefield = new Battlefield({
+        element: document,
+        screenHeight: window.innerHeight,
+        screenWidth: window.innerWidth,
+        tileHeight: 32,
+        tileWidth: 64,
+        map: "test",
+    })
+    document.querySelector(".mainMenu").style.display = "none"
+    battlefield.mapLoad()
+}
