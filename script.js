8e9c595f401280eb17b0dcaa67eafbe826d0167d
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-09 07:40:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,49 +3,39 @@ class battlefield {
     this.element = config.element;
     this.canvas = this.element.querySelector(".battlefield");
     this.ctx = this.canvas.getContext("2d");
+    this.screenHeight = config.screenHeight;
+    this.screenWidth = config.screenWidth;
+    this.tileHeight = config.tileHeight;
+    this.tileWidth = config.tileWidth;
   }
   
-  const screenHeight = window.screen.height;
-  const screenWidth = window.screen.width;
-  
-  mapLoad(test)
-  .then(resolve => {
-    const mapData = resolve.mapData
-    const tileImages = resolve.tileImages
-  })
-  
-  drawMap()
-  
-  async function mapLoad(map) {
+  async mapLoad(map) {
+    const self = this
     const response = await fetch (`maps/${map}.json`);
     const mapData = await response.json();
     return new Promise(function(resolve){
       let loadedTiles = 0
+      let tileImages = {}
       mapData.usedTiles.forEach(function(usedTile){
-        img = new Image;
-        img.src = `${usedTile}.png`;
+        const img = new Image;
+        img.src = `res.tiles.${usedTile}.png`;
         img.onload = function () {
           loadedTiles++
           tileImages[usedTile] = img
-          if (loadedTiles === mapData.usedTiles.length) {
-            resolve(mapData, tileImages)
+          if (loadedTiles === mapData.usedTiles.length){
+            //Map Drawing
+            for (let y=0; y<mapData.height; y++) {
+              for (let x=0; x<mapData.width; x++) {
+                const isoX = ((x-y-1)*self.tileWidth + self.screenWidth)/2;
+                const isoY = ((x+y)*self.tileHeight)/2;
+                const tile = mapData.tiles[x][y];
+                self.ctx.drawImage(tileImages[tile],isoX,isoY)
+              }
+            }
+            resolve(mapData)
           }
         }
       }
-      
-      }
-  }
-  
-  async function drawMap () {
-    const tileWidth = 64;
-    const tileHeight = 32;
-    for (let y=0, y>=mapData.height, y++) {
-      for (let x=0, x>=mapData.width, x++) {
-        const isoX = ((x-y-1)*tileWidth + screenWidth)/2;
-        const isoY = ((x+y)*tileHeight)/2;
-        const tile = mapData.tiles[x][y];
-        config.ctx.drawImg(tileImages[tile],isoX,isoY)
-      }
-    }
+    })
   }
 }
