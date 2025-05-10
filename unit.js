cdb7fb491082b5afc96affdce2c97cf1d93eba6a
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-10 11:40:15 +0300

    Update unit.js

--- unit.js
+++ unit.js
@@ -1,5 +1,21 @@
 class unit {
     constructor(config){
-        this.name = config.name
+        this.ID = config.ID;
+        this.team = config.team;
+        this.x = config.x;
+        this.y = config.y;
+        this.maxHP = confix.maxHP;
+        this.HP = config.HP;
+        this.moveToX = config.moveToX;
+        this.moveToY = config.moveToY;
+        this.fakeTeam = config.fakeTeam;
+        this.image = config.image;
+        this.canvas = document.querySelector(#battlefield);
+        this.ctx = canvas.ctx("2d");
+    }
+    drawUnit (){
+        const isoX = ((x-y-1) + screenWidth)/2;
+        const isoY = (x+y)/2;
+        ctx.drawImage(image, isoX, isoY);
     }
 }
