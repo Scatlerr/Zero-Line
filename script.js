70f46025c53c6d572d7e997fba5f3eb3d6d4c5b3
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-10 18:25:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
 class Terrain {
   constructor (config) {
-    this.element = config.element;
-    this.canvas = this.element.querySelector(".battlefield");
+    this.canvas = document.element.querySelector(".battlefield");
     this.ctx = this.canvas.getContext("2d");
-    this.screenHeight = config.screenHeight;
-    this.screenWidth = config.screenWidth;
     this.tileHeight = config.tileHeight;
     this.tileWidth = config.tileWidth;
     this.map = config.map;
-    this.canvas.width = this.screenWidth;
-    this.canvas.height = this.screenHeight;
+    this.canvas.width = (this.map.width+this.map.height)*this.tileWidth;
+    this.canvas.height = (this.map.width+this.map.height)*this.tileHeight;
   }
   
-  async mapLoad() {
-    console.log(this);
-    const self = this;
-    const response = await fetch (`res/maps/${this.map}.json`);
-    const mapData = await response.json();
-    return new Promise(function(resolve){
-      let loadedTiles = 0;
-      let tileImages = {};
-      mapData.usedTiles.forEach(function(usedTile){
-        const img = new Image;
-        img.src = `res/img/tiles/${usedTile}.png`;
-        img.onload = function () {
-          loadedTiles++;
-          tileImages[usedTile] = img;
-          if (loadedTiles === mapData.usedTiles.length){
-            //Map Drawing
-            for (let y=0; y<mapData.height; y++) {
-              for (let x=0; x<mapData.width; x++) {
-                const isoX = ((x-y-1)*self.tileWidth + self.screenWidth)/2;
-                const isoY = ((x+y)*self.tileHeight)/2;
-                const tile = mapData.tiles[y][x];
-                self.ctx.drawImage(tileImages[tile],isoX,isoY);
-              }
-            }
-            resolve(mapData);
-            self.canvas.style.display = "block"
-          }
-        }
+  async drawTerrain() {
+    this.map.tiles.forEach(function(usedTileRow, y){
+      usedTileRow.forEach(function(usedTile, x){
+        const isoX = ((x-y-1)*this.tileWidth + this.canvas.width)/2;
+        const isoY = ((x+y)*this.tileHeight)/2;
+        const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources[usedTile].texture)
       })
     })
   }
