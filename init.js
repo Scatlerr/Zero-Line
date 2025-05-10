88964fa4cb8183556152eef09f43d9697db86206
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-10 22:57:51 +0300

    Update init.js
    disable gestures

---
+++ init.js
@@ -0,0 +1,3 @@
+document.addEventListener('gesturestart', e => e.preventDefault());
+document.addEventListener('gesturechange', e => e.preventDefault());
+document.addEventListener('gestureend', e => e.preventDefault());
