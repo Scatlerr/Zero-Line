9fe54c2c1ced078505138137e55f5cf11bcb6036
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-11 12:30:33 +0300

    Update loadingScreen.js

--- loadingScreen.js
+++ loadingScreen.js
@@ -1,28 +1,29 @@
 //document.querySelector("loadingScreen").style.display="block"
+
 function loader (map){
     fetch(`./res/maps/${map}.json`)
     .then(response => response.json())
     .then(data => {
-        mapData = data;
+        const mapData = data;
         const app = new PIXI.Application({
             width: mapData.width,
             height: mapData.height
         })
-
-        const loader = PIXI.loader.shared
+        document.body.appendChild(app.view)
+        const loader = PIXI.Loader.shared
         mapData.usedTiles.forEach(usedTile =>{
             loader.add(usedTile, `./res/img/tiles/${usedTile}.png`)
         })
-        loader.load(loadingComplete)
-
-        function loadingComplete (){
+        mapData.units.forEach(usedUnit =>{
+            loader.add(`Units/${usedUnit}`, `./res/img/tiles/${usedUnit}.png`)
+        })
+        loader.load((loader,resources) => {
             const terrain = new Terrain({
                 tileHeight: 32,
                 tileWidth: 64,
                 mapData: mapData
             })
-            terrainRenderer(mapData)
+            terrainRenderer(mapData, app)
         }
-
     })
 }
