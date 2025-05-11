65d1cd897d2b829291c81804af6639ee533d344f
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-11 08:23:04 +0300

    Update terrainRenderer.js
    fixed for the new pixi approach

--- terrainRenderer.js
+++ terrainRenderer.js
@@ -1,21 +1,20 @@
 class Terrain {
   constructor (config) {
-    this.canvas = document.querySelector(".battlefield");
-    this.ctx = this.canvas.getContext("2d");
-    this.tileHeight = config.tileHeight;
-    this.tileWidth = config.tileWidth;
     this.map = config.map;
-    this.canvas.width = (this.map.width+this.map.height)*this.tileWidth;
-    this.canvas.height = (this.map.width+this.map.height)*this.tileHeight;
   }
   
   drawTerrain() {
-    this.map.tiles.forEach(usedTileRow, y =>{
-      usedTileRow.forEach(usedTile, x =>{
-        const isoX = ((x-y-1)*this.tileWidth + this.canvas.width)/2;
-        const isoY = ((x+y)*this.tileHeight)/2;
-        const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources[usedTile].texture)
+    this.map.tiles.forEach((usedTileRow, y) =>{
+      usedTileRow.forEach((usedTile, x) =>{
+        const isoX = ((x-y-1)*64 + app.width)/2;
+        const isoY = ((x+y)*32)/2;
+        const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources["Terrain"][usedTile].texture)
+        sprite.x = isoX
+        sprite.y = isoY
+        app.stage.addChild(sprite)
       })
     })
+    app.renderer.render(app.stage)
+    app.ticker.stop()
   }
 }
