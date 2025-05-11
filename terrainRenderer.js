f253bbb36f583b5dbc8b172f518f541b126d7537
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-11 08:44:25 +0300

    Update terrainRenderer.js

--- terrainRenderer.js
+++ terrainRenderer.js
@@ -1,20 +1,14 @@
-class Terrain {
-  constructor (config) {
-    this.map = config.map;
-  }
-  
-  drawTerrain() {
-    this.map.tiles.forEach((usedTileRow, y) =>{
-      usedTileRow.forEach((usedTile, x) =>{
-        const isoX = ((x-y-1)*64 + app.width)/2;
-        const isoY = ((x+y)*32)/2;
-        const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources["Terrain"][usedTile].texture)
-        sprite.x = isoX
-        sprite.y = isoY
-        app.stage.addChild(sprite)
-      })
+function drawTerrain(map) {
+  map.tiles.forEach((usedTileRow, y) =>{
+    usedTileRow.forEach((usedTile, x) =>{
+      const isoX = ((x-y-1)*64 + app.width)/2;
+      const isoY = ((x+y)*32)/2;
+      const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources["Terrain"][usedTile].texture)
+      sprite.x = isoX
+      sprite.y = isoY
+      app.stage.addChild(sprite)
     })
-    app.renderer.render(app.stage)
-    app.ticker.stop()
-  }
+  })
+  app.renderer.render(app.stage)
+  app.ticker.stop()
 }
