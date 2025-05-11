26d77e08bfd78ae5e79613720410880a1361429a
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-11 14:18:10 +0300

    Update init.js
    moved mainMenu.js to init.js

--- init.js
+++ init.js
@@ -1,3 +1,21 @@
 document.addEventListener('gesturestart', e => e.preventDefault());
 document.addEventListener('gesturechange', e => e.preventDefault());
 document.addEventListener('gestureend', e => e.preventDefault());
+
+const mainMenu = document.querySelector("#mainMenu")
+const campMenu = document.querySelector("#campMenu")
+
+document.querySelector("#campBtn").onclick = () =>{
+    mainMenu.style.display = "none";
+    campMenu.style.display = "flex";
+}
+
+document.querySelector("#backCampBtn").onclick = () =>{
+    campMenu.style.display = "none";
+    mainMenu.style.display = "flex"
+}
+
+document.querySelector("#startCampBtn").onclick = () => {
+    campMenu.style.display = "none"
+    loader("test")
+}
