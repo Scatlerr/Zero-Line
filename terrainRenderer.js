d6f8b10d327d95943ac58010d5d8f6ace805ae93
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-11 09:11:09 +0300

    Update terrainRenderer.js

--- terrainRenderer.js
+++ terrainRenderer.js
@@ -1,14 +1,27 @@
-function drawTerrain(map) {
+const terrainLayer = new PIXI.container();
+const groundLayer = new PIXI.container();
+const airLayer = new PIXI.container();
+
+app.stage.appendChild(terrainLayer);
+app.stage.appendChild(groundLayer);
+app.stage.appendChild(airLayer);.
+
+function terrainRenderer(map) {
   map.tiles.forEach((usedTileRow, y) =>{
     usedTileRow.forEach((usedTile, x) =>{
       const isoX = ((x-y-1)*64 + app.width)/2;
       const isoY = ((x+y)*32)/2;
-      const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources["Terrain"][usedTile].texture)
-      sprite.x = isoX
-      sprite.y = isoY
-      app.stage.addChild(sprite)
+      const sprite = new PIXI.Sprite(PIXI.Loader.shared.resources["Terrain"][usedTile].texture);
+      sprite.x = isoX;
+      sprite.y = isoY;
+      terrainLayer.addChild(sprite);
     })
   })
-  app.renderer.render(app.stage)
-  app.ticker.stop()
+  terrainLayer.cacheAsBitmap = true;
+}
+
+function unitRenderer(unit) {
+  unit.sprite.x = ((unit.x-unit.y-1)*64 + app.width)/2;
+  unit.sprite.y = ((unit.x+unit.y)*32)/2
+  groundLayer.addChild(sprite)
 }
