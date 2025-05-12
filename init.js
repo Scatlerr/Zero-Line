3c3f9584b310d87404cd33c52eaf27330353bc1d
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-12 11:41:43 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,5 +1,3 @@
-logToScreen(`Canvas size: ${app.renderer.width} x ${app.renderer.height}`);
-
 document.addEventListener('gesturestart', e => e.preventDefault());
 document.addEventListener('gesturechange', e => e.preventDefault());
 document.addEventListener('gestureend', e => e.preventDefault());
