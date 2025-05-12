252e772917dddd9a5a7c880ea577ecee99f3f5d2
Scatlerr <132938051+Scatlerr@users.noreply.github.com>
2025-05-12 11:44:39 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,3 +1,5 @@
+consoleToLog("bye")
+
 document.addEventListener('gesturestart', e => e.preventDefault());
 document.addEventListener('gesturechange', e => e.preventDefault());
 document.addEventListener('gestureend', e => e.preventDefault());
